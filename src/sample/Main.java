4b4b187ded50f8543b725b20b646a7af6b4e1849
brien17 <43622552+brien17@users.noreply.github.com>
2019-09-13 18:06:20 -0400

    Fixed checkstyle issues

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * This is the main method of my program.
+ *
+ * @author cam
+ */
 public class Main extends Application {
+  /**
+   * This method is the starting point for my Java FX program.
+   *
+   * @param primaryStage the first thing the user sees
+   * @throws Exception potential exception
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Hello World");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
