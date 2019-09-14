dcd71cc50003ee9da8d32cdf6a8c9546f38d2262
cbrien17 <cbrien0661@eagle.fgcu.edu>
2019-09-13 22:47:02 -0400

    Added functions for clicking on the buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,28 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
+public class Controller {
+  @FXML private Button productionLineButton;
+
+  @FXML private Button recordProductionButton;
+
+  @FXML private Button productionLogButton;
+
+  @FXML
+  void productionLineButtonAction(MouseEvent event) {
+    System.out.println("production line button clicked");
+  }
+
+  @FXML
+  void productionLogButtonAction(MouseEvent event) {
+    System.out.println("production log button clicked");
+  }
+
+  @FXML
+  void recordProductionButtonAction(MouseEvent event) {
+    System.out.println("record production button clicked");
+  }
 }
