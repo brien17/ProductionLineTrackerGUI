57c2150ca6fcea50ce183df07548d32d6fd135bf
brien17 <43622552+brien17@users.noreply.github.com>
2019-10-03 13:40:51 -0400

    Updated audio player with issue 2 requirements

--- src/sample/AudioPlayer.java
+++ src/sample/AudioPlayer.java
@@ -8,28 +8,34 @@ public class AudioPlayer extends Product implements MultimediaControl {
   // methods
 
   // constructor
-  public AudioPlayer(String name, String manufacturer, String audioSpecification) {
-    super(name, manufacturer, "AU");
+  public AudioPlayer(
+      String name, String manufacturer, String audioSpecification, String mediaType) {
+    super(name, manufacturer, "AUDIO");
     this.audioSpecification = audioSpecification;
+    this.mediaType = mediaType;
   }
 
   public void play() {
-    System.out.println("play");
+    System.out.println("Playing");
   }
 
   public void stop() {
-    System.out.println("stop");
+    System.out.println("Stopping");
   }
 
   public void previous() {
-    System.out.println("previous");
+    System.out.println("Previous");
   }
 
   public void next() {
-    System.out.println("next");
+    System.out.println("Next");
   }
 
   public String toString() {
-    return super.toString() + "\nAudio Spec: " + audioSpecification + "\nMedia Type: " + mediaType;
+    return super.toString()
+        + "\nSupported Audio Formats: "
+        + audioSpecification
+        + "\nSupported Playlist Formats: "
+        + mediaType;
   }
 }
